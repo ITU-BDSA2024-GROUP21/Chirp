35e966f9b999fcc63573df24b2e861afd679c005
kspi <kspi@itu.dk>
2024-08-28 16:16:33 +0200

    CSV Parser Co-authored-by: Andreas lawu@itu.dk, Mathilde msem@itu.dk, Stine stwp@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+using (var reader = new StreamReader("chirp_cli_db.csv"))
+{
+    List<String> Authors = new List<String>();
+    List<String> cheeps = new List<String>();
+    List<long> time  = new List<long>();
+    int csvLength = System.IO.File.ReadAllLines("chirp_cli_db.csv").Length;
+    
+    Console.WriteLine(csvLength);
+    foreach (var i in Enumerable.Range(0, csvLength))
+    {
+        var line = reader.ReadLine();
+        if (i == 0)
+            continue;
+        
+        var values = line.Split(',');
+        Authors.Add(values[0]);
+        cheeps.Add(values[1]);
+        time.Add(long.Parse(values[2]));
+    }
+    Console.WriteLine(Authors[0]);
+    
+}
+

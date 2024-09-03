e19088bb871dfa303d049a3d87d2317c543cac95
kspi <kspi@itu.dk>
2024-09-03 14:45:55 +0200

    Cheeps to CSV Co-authored-by: Andreas lawu@itu.dk, Stine stwp@itu.dk, Mathilde msem@itu.dk

--- Chirp.CLI/cheep.cs
+++ Chirp.CLI/cheep.cs
@@ -1,13 +1,18 @@
 ﻿namespace Chirp.CLI;
 using System;  
 public class cheep {
-    static String skeet = "";
+    static String input = "";
+    private String csvFilePath = "chirp_cli_db.csv";
     public static void test(string[] args)
     {
         Console.WriteLine("make Cheep");
-        skeet = Console.ReadLine();
-        Console.WriteLine(skeet);
-        
+        input = Console.ReadLine();
+        Console.WriteLine(input + " " + Environment.UserName + " " + DateTimeOffset.Now.ToUnixTimeSeconds());
+        string path = @"chirp_cli_db.csv";
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine(Environment.UserName + ",\"" + input + "\"," + DateTimeOffset.Now.ToUnixTimeSeconds());
+        }
     }
     
 }

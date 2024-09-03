f89e3b0091f28c662639afd4b5e9e0d4b168fe21
kspi <kspi@itu.dk>
2024-09-03 14:25:09 +0200

    Match class case Co-authored-by: Andreas lawu@itu.dk, Stine stwp@itu.dk, Mathilde msem@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,33 +1,56 @@
 ﻿using System.IO;
 using System.Text.RegularExpressions;
+using Chirp.CLI;
 
-using (var reader = new StreamReader("chirp_cli_db.csv"))
-{
-
-    List<String> authors = new List<String>();
-
-    List<String> cheeps = new List<String>();
-    List<DateTime> time = new List<DateTime>();
-    int csvLength = System.IO.File.ReadAllLines("chirp_cli_db.csv").Length;
-
-    foreach (var i in Enumerable.Range(0, csvLength))
+class Program {
+    public static void Main(string[] args)
     {
-        var line = reader.ReadLine();
-        if (i == 0)
-            continue;
-
-        if (line == null)
-            continue;
-
-        var values = Regex.Split(line, "[,]{1}(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
-
-        authors.Add(values[0]);
-        cheeps.Add(values[1]);
-        time.Add(DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2])).AddHours(2).DateTime);
+        if (args.Length > 0)
+        {
+            switch (args[0])
+            {
+                case "cheep":
+                    cheep.test(args);
+                    return;
+                case "read":
+                    readCSV(args);
+                    return;
+            }
+        }
     }
 
-    foreach (var i in Enumerable.Range(0, csvLength - 1))
-    {
-        Console.WriteLine(authors[i] + " @ " + time[i].ToString("MM/dd/yyyy HH:mm:ss") + ": " + cheeps[i]);
+    static void readCSV(string[] args)
+        {
+
+            using (var reader = new StreamReader("chirp_cli_db.csv"))
+            {
+
+                List<String> authors = new List<String>();
+
+                List<String> cheeps = new List<String>();
+                List<DateTime> time = new List<DateTime>();
+                int csvLength = System.IO.File.ReadAllLines("chirp_cli_db.csv").Length;
+
+                foreach (var i in Enumerable.Range(0, csvLength))
+                {
+                    var line = reader.ReadLine();
+                    if (i == 0)
+                        continue;
+
+                    if (line == null)
+                        continue;
+
+                    var values = Regex.Split(line, "[,]{1}(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
+
+                    authors.Add(values[0]);
+                    cheeps.Add(values[1]);
+                    time.Add(DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2])).AddHours(2).DateTime);
+                }
+
+                foreach (var i in Enumerable.Range(0, csvLength - 1))
+                {
+                    Console.WriteLine(authors[i] + " @ " + time[i].ToString("MM/dd/yyyy HH:mm:ss") + ": " + cheeps[i]);
+                }
+            }
+        }
     }
-}
--- Chirp.CLI/cheep.cs
+++ Chirp.CLI/cheep.cs
@@ -1,6 +1,13 @@
 ﻿namespace Chirp.CLI;
-
-public class cheep
-{
+using System;  
+public class cheep {
+    static String skeet = "";
+    public static void test(string[] args)
+    {
+        Console.WriteLine("make Cheep");
+        skeet = Console.ReadLine();
+        Console.WriteLine(skeet);
+        
+    }
     
 }

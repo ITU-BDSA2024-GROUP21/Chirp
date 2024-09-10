46a046b1341e6bbf58bb2ce798b03e6095c6339a
Andreas <lawu@itu.dk>
2024-09-10 16:38:52 +0200

    Databases after refactoring Co-authored-by: Stine <stwp@itu.dk> Co-authored-by: Mathilde <msem@itu.dk> Co-authored-by: Kristoffer <kspi@itu.dk>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,6 +1,29 @@
-namespace DefaultNamespace;
+namespace SimpleDB;
+using System;
+using System.Globalization;
+using CsvHelper;
 
-public class CSVDatabase
+public record Cheep(string Author, string Message, long Timestamp);
+
+public sealed class CSVDatabase : IDatabaseRepository<Cheep>
 {
+    public IEnumerable<Cheep> Read(int? limit = null)
+    {
+        if (limit.HasValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        StreamReader reader = new StreamReader("../SimpleDB/chirp_cli_db.csv"); 
+        CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
     
+        {
+            var records = csv.GetRecords<Cheep>();
+            return records;
+        }
+    }
+    
+    public void Store(Cheep record)
+    {
+    }
 }

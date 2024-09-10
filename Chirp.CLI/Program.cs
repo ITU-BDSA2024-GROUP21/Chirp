960aa5b0680e264ffccca47d78152a6dbd0140c6
Andreas <lawu@itu.dk>
2024-09-10 18:00:32 +0200

    Almost kinda works with commandline ima run byeeeeee Co-authored-by: Stine <stwp@itu.dk> Co-authored-by: Mathilde <msem@itu.dk> Co-authored-by: Kristoffer <kspi@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -5,11 +5,42 @@ using System.Globalization;
 using CsvHelper;
 using System.Collections.Generic;
 using SimpleDB;
+using CommandLine;
 
 class Program
 {
+    public class Options
+    {
+        [Option('c', "cheep", Required = true, HelpText = "Write your cheep.")]
+        public bool Cheep { get; set; }
+        
+        [Option('r', "read", Required = true, HelpText = "Read all cheeps.")]
+        public bool Read { get; set; }
+    }
+    
     public static void Main(string[] args)
     {
+        var database = new CSVDatabase();
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                if (o.Cheep)
+                {
+                    var input = Console.ReadLine();
+                    var cheep = new SimpleDB.Cheep(Environment.UserName, input, DateTimeOffset.Now.ToUnixTimeSeconds());
+                    database.Store(cheep);
+                }
+                if (o.Read)
+                {
+                    var records = database.Read();
+                    UserInterface.PrintCheeps(records);
+                }
+                else
+                {
+                    Console.WriteLine("Write a proper command");
+                }
+            });
+        /*
         var database = new CSVDatabase();
         if (args.Length > 0)
         {
@@ -29,6 +60,7 @@ class Program
                     return;
             }
         }
+        */
     }
 }
     

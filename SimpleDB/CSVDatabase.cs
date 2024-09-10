637410eac20f5ba2cd7b326a96eb436f41c15c34
Andreas <lawu@itu.dk>
2024-09-10 18:16:19 +0200

    Commandline for database Co-authored-by: Stine <stwp@itu.dk> Co-authored-by: Mathilde <msem@itu.dk> Co-authored-by: Kristoffer <kspi@itu.dk>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -6,7 +6,8 @@ using CsvHelper;
 public record Cheep(string Author, string Message, long Timestamp);
 
 public sealed class CSVDatabase : IDatabaseRepository<Cheep>
-{
+{   
+    public string csvPath = "../SimpleDB/chirp_cli_db.csv";
     public IEnumerable<Cheep> Read(int? limit = null)
     {
         if (limit.HasValue)
@@ -14,7 +15,7 @@ public sealed class CSVDatabase : IDatabaseRepository<Cheep>
             throw new NotImplementedException();
         }
 
-        StreamReader reader = new StreamReader("../SimpleDB/chirp_cli_db.csv"); 
+        StreamReader reader = new StreamReader(csvPath); 
         CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
     
         {
@@ -24,6 +25,10 @@ public sealed class CSVDatabase : IDatabaseRepository<Cheep>
     }
     
     public void Store(Cheep record)
-    {
+    {   
+        using (StreamWriter sw = File.AppendText(csvPath))
+        {
+            sw.WriteLine(record.Author + ",\"" + record.Message + "\"," + record.Timestamp);
+        }
     }
 }

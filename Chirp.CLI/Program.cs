89b6ca2d64c79b603fa7d4af64f35ff5f4ea928a
Andreas <lawu@itu.dk>
2024-09-10 14:04:17 +0200

    Formatted output correctly when reading CSV  Co-authored-by: Stine <stwp@itu.dk> >> Co-authored-by: Mathilde <msem@itu.dk> >> Co-authored-by: Kristoffer <kspi@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -26,47 +26,17 @@ class Program {
 
     static void readCSV(string[] args)
 
-    {
-            using (var reader = new StreamReader("chirp_cli_db.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {   
+        using (var reader = new StreamReader("chirp_cli_db.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<Cheep>();
+            foreach (var item in records)
             {
-                var records = csv.GetRecords<Cheep>();
-                foreach (var item in records)
-                    Console.WriteLine(item);
+                var timestamp = (int) item.Timestamp;
+                var dateTime = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+                Console.WriteLine(item.Author + " @ " + dateTime.ToString("MM/dd/yyyy HH:mm:ss") + ": " + item.Message);
             }
-            
-            
-
-            /*
-            {
-
-                List<String> authors = new List<String>();
-
-                List<String> cheeps = new List<String>();
-                List<DateTime> time = new List<DateTime>();
-                int csvLength = System.IO.File.ReadAllLines("chirp_cli_db.csv").Length;
-
-                foreach (var i in Enumerable.Range(0, csvLength))
-                {
-                    var line = reader.ReadLine();
-                    if (i == 0)
-                        continue;
-
-                    if (line == null)
-                        continue;
-
-                    var values = Regex.Split(line, "[,]{1}(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
-
-                    authors.Add(values[0]);
-                    cheeps.Add(values[1]);
-                    time.Add(DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2])).AddHours(2).DateTime);
-                }
-
-                foreach (var i in Enumerable.Range(0, csvLength - 1))
-                {
-                    Console.WriteLine(authors[i] + " @ " + time[i].ToString("MM/dd/yyyy HH:mm:ss") + ": " + cheeps[i]);
-                }
-            }
-            */
         }
     }
+}

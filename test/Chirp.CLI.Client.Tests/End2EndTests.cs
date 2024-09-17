b7f748df8b5f4a4511e496d892fcee1c4f30e4dc
kspi <kspi@itu.dk>
2024-09-17 22:39:54 +0200

    End-2-End test for printing

---
+++ test/Chirp.CLI.Client.Tests/End2EndTests.cs
@@ -0,0 +1,30 @@
+namespace Chirp.CLI.Client.Tests.End2EndTests;
+using Chirp;
+using System.Diagnostics;
+using System;
+using SimpleDB;
+
+
+public class End2EndTests
+{
+    [Fact]
+    public void testOutput()
+    {
+		var database = new CSVDatabase();
+        string csvPath = "../../data/chirp_cli_db.csv";
+        database.csvPath = csvPath;
+        var cheeps = database.Read().Take(3).ToList();
+        var expectedOutput = "ropf @ 08-01-2023 12:09:20: Hello, BDSA students!\r\n" + 
+                             "adho @ 08-02-2023 12:19:38: Welcome to the course!\r\n" +
+                             "adho @ 08-02-2023 12:37:38: I hope you had a good summer.\r\n";
+        using (var sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+            UserInterface.PrintCheeps(cheeps);
+            var output = sw.ToString();
+            Assert.Equal(expectedOutput, output);
+        }
+
+    }
+        
+}

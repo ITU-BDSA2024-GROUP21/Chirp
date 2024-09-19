a7efafae361e5e1f0bdfdf971cb774758a82bab2
Andreas <lawu@itu.dk>
2024-09-19 19:00:33 +0200

    Deleted unecessairy command line Co-authored-by: Stine <stwp@itu.dk> Co-authored-by: Mathilde <msem@itu.dk> Co-authored-by: Kristoffer <kspi@itu.dk>

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -13,7 +13,6 @@ public class timeConverterTests
     {
         var dateAndTime = new DateTimeOffset(2008, 5, 1, 8, 6, 32,
             new TimeSpan(1, 0, 0));
-        Console.WriteLine(dateAndTime);
         long convertedDateAndTime = timeConverter.ConvertToUnixTime(dateAndTime);
         
         Assert.Equal((long)1209625592, convertedDateAndTime);

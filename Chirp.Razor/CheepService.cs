bda54f91805894ade8919ae9fb02535e6618a915
stwp <stwp@itu.dk>
2024-10-01 15:59:49 +0200

    Solution file + Cheep services co-authored-by: Mathilde <msem@itu.dk> co-authored-by: Kristoffer <kspi@itu.dk> co-authored-by: Andreas <lawu@itu.dk>

--- Chirp.Razor/CheepService.cs
+++ Chirp.Razor/CheepService.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
+
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
 public interface ICheepService
@@ -8,15 +11,46 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
+    string _sqlDBFilePath = "/../chirp.db";
+    string sqlQuery = @"SELECT author, message, pub_date FROM message ORDER BY pub_date DESC";
+
     // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
-        {
+    private static readonly List<CheepViewModel> _cheeps = new();
+        /*{
+            
             new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
             new CheepViewModel("Adrian", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
-        };
+        }*/
+    
 
     public List<CheepViewModel> GetCheeps()
     {
+        //var sqlDBFilePath = "../chirp.db";
+        //var sqlQuery = @"SELECT author, message, pub_date JOIN FROM message ORDER BY pub_date DESC";
+        
+        using (var connection = new SqliteConnection($"Data Source={_sqlDBFilePath}"))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+
+            try
+            {
+                command.CommandText = sqlQuery;
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var author = reader.GetString(0);
+                    var message = reader.GetString(1);
+                    var timestamp = reader.GetString(2);
+
+                    var cheep = new CheepViewModel(author, message, timestamp);
+                    _cheeps.Add(cheep);
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
         return _cheeps;
     }
 

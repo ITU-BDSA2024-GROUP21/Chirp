9a4d27df327d7da05f04ab8a0c35c45b00540b45
stwp <stwp@itu.dk>
2024-10-07 11:01:44 +0200

    Added working tests

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,71 +1,39 @@
 using Chirp.Razor;
-using Microsoft.Data.Sqlite;
 
-var builder = WebApplication.CreateBuilder(args);
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
 
- var chirpDbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") 
-                  ?? Path.Combine(Path.GetTempPath(), "chirp.db");
+        var builder = WebApplication.CreateBuilder(args);
+
+        var chirpDbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH")
+                          ?? Path.Combine(Path.GetTempPath(), "chirp.db");
 
 
 // Add services to the container.
-string path = "../chirp.db";
-/*
-if (Environment.GetEnvironmentVariable("CHIRPDBPATH") != null)
-{
-    path = Environment.GetEnvironmentVariable("CHIRPDBPATH");
-} else{
-    path = Path.Combine("C:\\Users\\krist\\RiderProjects\\Chirp\\chirp.db");
-    Console.WriteLine(path);
-}
-if (!File.Exists(path))
-{
-    newDatabase(path);
-}
-*/
+        builder.Services.AddRazorPages();
+        builder.Services.AddSingleton<ICheepService, CheepService>();
+        builder.Services.AddTransient<DBFacade>(_ => new DBFacade(chirpDbPath));
 
-builder.Services.AddSingleton<DBFacade>(_ => new DBFacade(path));
 
-void newDatabase(string path)
-{
-    using var connection = new SqliteConnection($"Data Source={path}");
-    connection.Open();
-
-    var command = connection.CreateCommand();
-    command.CommandText = @"
-
-    create table if not exists user (
-        user_id integer primary key autoincrement,
-        username string not null,
-        email string not null,
-        pw_hash string not null
-                                      );
-    create table if not exists message (
-        message_id integer primary key autoincrement,
-        author_id integer not null,
-        text string not null,
-        pub_date integer
-                                    
-    );";
-    command.ExecuteNonQuery();
-}
-
-builder.Services.AddSingleton<ICheepService, CheepService>();
-
-var app = builder.Build();
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapRazorPages();
-
-app.Run();

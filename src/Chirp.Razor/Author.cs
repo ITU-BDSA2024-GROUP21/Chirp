4129ad54022eb1d6b6291fe737689adee062912d
kspi <kspi@itu.dk>
2024-10-08 12:55:56 +0200

    fix Cheep and Author classes

--- src/Chirp.Razor/Author.cs
+++ src/Chirp.Razor/Author.cs
@@ -4,13 +4,6 @@ public class Author
 {
     string Name { get; set; }
     string Email { get; set; }
-    
+
     ICollection<Cheep> Cheeps { get; set; }
-    
-    public Author(string name, string email)
-    {
-        Name = name;
-        Email = email;
-        Cheeps = new List<Cheep>();
-    }
 }
--- src/Chirp.Razor/Cheep.cs
+++ src/Chirp.Razor/Cheep.cs
@@ -4,16 +4,7 @@ public class Cheep
 {
     string Text { get; set; }
     DateTime Timestamp { get; set; }
-    
+
     Author author { get; set; }
-    
-    public Cheep(string text, DateTime timestamp, Author author)
-    {
-        Text = text;
-        Timestamp = timestamp;
-        this.author = author;
-        author.cheeps.Add(this);
-        
-    }
-    
+
 }

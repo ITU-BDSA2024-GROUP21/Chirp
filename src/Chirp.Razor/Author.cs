be777b9f7df05c35b886ab5c7b0fb61569b8701c
kspi <kspi@itu.dk>
2024-10-08 11:48:46 +0200

    Added Cheep and Author classes
    Co-authored-by: Mathilde <msem@itu.dk>
    Co-authored-by: Stine <stwp@itu.dk>

---
+++ src/Chirp.Razor/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Chirp.Razor;
+
+public class Author
+{
+    string Name { get; set; }
+    string Email { get; set; }
+    
+    ICollection<Cheep> Cheeps { get; set; }
+}
---
+++ src/Chirp.Razor/Cheep.cs
@@ -0,0 +1,10 @@
+﻿namespace Chirp.Razor;
+
+public class Cheep
+{
+    string Text { get; set; }
+    DateTime Timestamp { get; set; }
+    
+    Author author { get; set; }
+    
+}

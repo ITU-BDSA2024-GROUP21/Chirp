36783eb27383aa2feeb16288cd6093ecd4fda377
Andreas <lawu@itu.dk>
2024-10-16 20:56:11 +0200

    Added another dotnet 8 to migrations

--- src/Chirp.Razor/Migrations/ChirpDBContextModelSnapshot.cs
+++ src/Chirp.Razor/Migrations/ChirpDBContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Chirp.Razor.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "7.0.20");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
 
             modelBuilder.Entity("Chirp.Razor.Author", b =>
                 {

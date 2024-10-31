5cd8a816ccc301243d0d769f115f1b4357c13870
kspi <kspi@itu.dk>
2024-10-31 16:22:48 +0100

    fixed so wrong mail doesn't crash the site
    Co-authored-by: Mathilde <msem@itu.dk>
    Co-authored-by: Stine <stwp@itu.dk>

--- src/Chirp.Razor/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Chirp.Razor/Chirp.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,6 +115,11 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var user = await _userManager.FindByEmailAsync(Input.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
                 
                 var result = await _signInManager.PasswordSignInAsync(user, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)

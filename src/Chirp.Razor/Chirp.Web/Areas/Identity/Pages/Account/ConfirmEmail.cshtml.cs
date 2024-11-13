864058212d27d7093e3a4bb809d569fafce09f18
MathildeMarcussen <msem@itu.dk>
2024-11-13 19:04:08 +0100

    Register Fix, so now after you have registered you are automaticcaly logged in

--- src/Chirp.Razor/Chirp.Web/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ src/Chirp.Razor/Chirp.Web/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -16,11 +16,15 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
 {
     public class ConfirmEmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<ConfirmEmailModel> _logger;
 
-        public ConfirmEmailModel(UserManager<IdentityUser> userManager)
+        public ConfirmEmailModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<ConfirmEmailModel> logger)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
         }
 
         /// <summary>
@@ -29,7 +33,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
         /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        public async Task<IActionResult> OnGetAsync(string userId, string code, string returnUrl = null)
         {
             if (userId == null || code == null)
             {
@@ -39,13 +43,28 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
+                _logger.LogWarning($"Unable to find user with ID '{userId}' for email confirmation");
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
 
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            var decodedCode = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, decodedCode);
+            
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User with ID '{UserID}' confirmed their email succesfully", userId);
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return LocalRedirect(returnUrl ?? "~/");
+
+            }
+            else
+            {
+                _logger.LogError("Error confirming email for user with ID '{UserId}': {Errors}", userId, result.Errors);
+            }
+
             return Page();
         }
     }
 }
+

6a6214a4aaff7ec810df3ae01c7edc337a3112c6
RejeSeje <andreasmwulff@gmail.com>
2024-11-24 23:07:38 +0100

    unit test for delete button

--- test/Chirp.Razor.Tests/PlaywrightTests/UnitTest1.cs
+++ test/Chirp.Razor.Tests/PlaywrightTests/UnitTest1.cs
@@ -43,7 +43,7 @@ public class UnitTest1 : PageTest
     }
     
     //NOTE: PROGRAM SHOULD BE RUNNING BEFORE RUNNING THE UI TEST
-
+    
     
     //NootBoxIsVisibleWhenloggedIn is testing that a nootchat is visible
     //when a user is logged in.
@@ -179,7 +179,7 @@ public class UnitTest1 : PageTest
         //Checks that the Noot-chat box is exiting
         await Expect(Page.Locator("#Text")).ToBeVisibleAsync();
         await Page.Locator("#Text").ClickAsync();
-        //Makesure the test passes everytime by adding a unique identifier like timestamp
+        //Make sure the test passes everytime by adding a unique identifier like timestamp
         string uniqueMessage = $"Hello, I am feeling good!<script>alert('If you see this in a popup, you are in trouble!');</script>\n\n - {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
         await Page.Locator("#Text").FillAsync(uniqueMessage);
         await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
@@ -212,7 +212,7 @@ public class UnitTest1 : PageTest
     }
     
     [Test]
-    public async Task Registerworks()
+    public async Task RegisterTest()
     {
         await Page.GotoAsync("https://localhost:5273/");
     
@@ -228,15 +228,44 @@ public class UnitTest1 : PageTest
         await Page.GetByLabel("Confirm Password").FillAsync("Halløj691!");
     
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-        var _content = await Page.ContentAsync();
-        Console.WriteLine(_content);
-        //await Page.WaitForURLAsync("**/RegisterConfirmation?email=*&returnUrl=*");
+        var content = await Page.ContentAsync();
+        
+        Console.WriteLine(content);
 
         
-        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" })).ToBeVisibleAsync();
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your account" })).ToBeVisibleAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your account" }).ClickAsync();
+        
+        // TODO: Needs to delete user again after test
     
     }
     
+    [Test]
+    public async Task DeleteTest()
+    {
+        await Page.GotoAsync("https://localhost:5273/");
+        
+        // Log in and navigate to users timeline
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("marcus@mail.dk");
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").FillAsync("Halløj1!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "My timeline" }).ClickAsync();
+        
+        // Make Noot and check for existence
+        await Page.Locator("#Text").ClickAsync();
+        string uniqueMessage = $"hej med dig!! - {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+        await Page.Locator("#Text").FillAsync(uniqueMessage);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "My timeline" }).ClickAsync(); // Not sure why but it is necessary manually to navigate to personal timeline again
+        var specificCheep = Page.Locator("ul#messagelist li").Filter(new() { HasText = uniqueMessage });
+        await Expect(specificCheep).ToBeVisibleAsync();
+        
+        // Delete Noot and check that it no longer exists on users timeline
+        await specificCheep.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+        await Expect(specificCheep).Not.ToBeVisibleAsync();
     
+    }
 }

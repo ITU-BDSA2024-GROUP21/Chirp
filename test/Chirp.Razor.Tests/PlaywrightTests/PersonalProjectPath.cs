1285aaacbcf4f126f2c4d766b238b554a08c380f
Andreas <lawu@itu.dk>
2024-11-25 10:40:24 +0100

    testing that personalprojectpath local changes are ignored

--- test/Chirp.Razor.Tests/PlaywrightTests/PersonalProjectPath.cs
+++ test/Chirp.Razor.Tests/PlaywrightTests/PersonalProjectPath.cs
@@ -4,6 +4,8 @@ public class PersonalProjectPath
 {
     // Personal absolute path to web app. Done this way to be able to include in gitignore
     public static string path = @"C:\Users\andre\dev\Chirp\src\Chirp.Razor\Chirp.Web";
+    
+    // Skippidy toilet
 
     public static string getPath()
     {

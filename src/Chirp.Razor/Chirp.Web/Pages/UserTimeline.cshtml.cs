3062e2eabe0c63d51eda351614c3b2636bc16faa
kspi <kspi@itu.dk>
2024-11-25 10:48:55 +0100

    Fixed so that when 'visiting' others timelines, only their noots should appear not the ones they follow

--- src/Chirp.Razor/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -11,12 +11,15 @@ public class UserTimelineModel : PageModel
     private int _page;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly ICheepRepository _cheepRepository;
+    private Dictionary<string, bool> FollowerMap;
+
 
     public UserTimelineModel(ICheepService cheepService, UserManager<ApplicationUser> userManager,ICheepRepository cheepRepository )
     {
         _cheepService = cheepService;
         _userManager = userManager;
         _cheepRepository = cheepRepository;
+        FollowerMap = new Dictionary<string, bool>();
     }
     
     public async Task<IActionResult> OnPostDeleteCheepAsync(int cheepId)
@@ -46,11 +49,30 @@ public class UserTimelineModel : PageModel
         {
             return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
         }
+
+        if (user.UserName != author)
+        {
+            Cheeps = await _cheepService.GetCheepsFromAuthor(author, _page);
+            
+        }
+        else
+        {
+            var followedAuthors = await _cheepService.GetFollowedAuthors(currentAuthor.AuthorId);
+            followedAuthors.Add(user.UserName);
         
-        var followedAuthors = await _cheepService.GetFollowedAuthors(currentAuthor.AuthorId);
-        followedAuthors.Add(user.UserName);
+            Cheeps = await _cheepService.GetCheepsFromFollowedAuthor(followedAuthors, _page);
+        }
+        Author _author = await _cheepService.GetAuthorByName(User.Identity.Name);
+        int id = _author.AuthorId;
+        if (User.Identity.IsAuthenticated)
+        {
+            foreach (var cheep in Cheeps)
+            {
+                FollowerMap[cheep.Author] = await _cheepService.IsFollowing(id, cheep.AuthorId);
+            }
+        }
+        ViewData["FollowerMap"] = FollowerMap;
         
-        Cheeps = await _cheepService.GetCheepsFromFollowedAuthor(followedAuthors, _page);
 
         return Page();
     }
@@ -85,18 +107,35 @@ public class UserTimelineModel : PageModel
     {
         return BitConverter.ToInt32(guid.ToByteArray(), 0);
     }
-    public async Task<IActionResult> OnPostUnfollow(int followingAuthorId, string followerAuthor, int page)
+    public async Task<IActionResult> OnPostFollow(int followingAuthorId, string followerAuthor, int page)
     {
-        if (string.IsNullOrEmpty(followerAuthor))
-        {
-            ModelState.AddModelError("", "User identity is not valid.");
-            return Redirect($"/?page={page}");
-        }
-        
         Author author = await _cheepService.GetAuthorByName(followerAuthor);
         int id = author.AuthorId;
         
+        if (string.IsNullOrEmpty(followerAuthor))
+        {
+            ModelState.AddModelError("", "User identity is not valid.");
+            return Redirect($"/{author.Name}");
+        }
+        
+        await _cheepRepository.FollowAuthor(id,followingAuthorId);
+        FollowerMap[author.Name] = true;
+        return Redirect($"/{author.Name}");
+    }
+    public async Task<IActionResult> OnPostUnfollow(int followingAuthorId, string followerAuthor, int page)
+    {
+        Author author = await _cheepService.GetAuthorByName(followerAuthor);
+        int id = author.AuthorId;
+        
+        if (string.IsNullOrEmpty(followerAuthor))
+        {
+            ModelState.AddModelError("", "User identity is not valid.");
+            return Redirect($"/{author.Name}");
+        }
+        
+        Console.WriteLine(followingAuthorId);
         await _cheepRepository.Unfollow(id,followingAuthorId);
+        FollowerMap[author.Name] = false;
         return Redirect($"/{author.Name}");
     }
     

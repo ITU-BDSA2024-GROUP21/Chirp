801e52b8d723aca947ca2ae26813e8e8e79e2ea8
stwp <stwp@itu.dk>
2024-11-27 12:58:31 +0100

    Deleted it again

--- src/Chirp.Razor/Chirp.Web/Program.cs
+++ src/Chirp.Razor/Chirp.Web/Program.cs
@@ -43,7 +43,6 @@ public partial class Program
         {
             var db = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
             db.Database.EnsureCreated();
-            db.Database.Migrate();
             DbInitializer.SeedDatabase(db);
         }
 

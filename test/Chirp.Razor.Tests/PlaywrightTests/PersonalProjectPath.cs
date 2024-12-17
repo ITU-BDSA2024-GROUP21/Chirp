bf7d34c89f7674c03200739cec5063d08d3f5c23
Andreas Wulff <andreasmwulff@gmail.com>
2024-12-17 13:27:43 +0100

    Added environment variable to be able to interact via github workflow

--- test/Chirp.Razor.Tests/PlaywrightTests/PersonalProjectPath.cs
+++ test/Chirp.Razor.Tests/PlaywrightTests/PersonalProjectPath.cs
@@ -1,13 +1,18 @@
-﻿namespace Chirp.Razor.Tests.PlaywrightTests;
+namespace Chirp.Razor.Tests.PlaywrightTests;
 
 public class PersonalProjectPath
 {
     // Personal absolute path to web app. Done this way to be able to include in gitignore
 
-    public static string path = "../../../../src/Chirp.Razor/Chirp.Web";
+    private static string defaultPath = @"src\Chirp.Razor\Chirp.Web";
+
+    private static string envVarName = "CHIRP_PROJECT_PATH";
+
+    public static string path = GetPath();
 
     public static string getPath()
     {
-        return path;
+        string customPath = Environment.GetEnvironmentVariable(envVarName);
+        return !string.IsNullOrEmpty(customPath) ? customPath : defaultPath;
     }
 }
